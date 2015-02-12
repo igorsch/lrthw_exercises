0bc94582f59e9cb712c5f5f85e8b253bff32e80b
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-11 18:21:06 -0700

    Added Chapter 10

---
+++ Chapter_10/Readme.md
@@ -0,0 +1,4 @@
+>We would use ''' instead of """ in a case where we're putting " inside our string.
+
+####No offenses with rubocop
+ 
---
+++ Chapter_10/ex10.rb
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = ''"
+I'll do a list:
+\t* Catfood
+\t* Fishies
+\t* Catnip\n\t* Grass
+"''
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+puts "\nMovie Title is\f\"Hero\"."
+puts 'I\'m fine. Thank you.'
+puts "This line is long, but you only see portion of it.'"
+puts "This should ring\a"
+puts "Does anyone knows what \u1535 mean?"
+puts "\110\105\114\114\117"
+puts "My name is \x49\x67\x6F\x72"
