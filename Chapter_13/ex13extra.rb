c06706974d900f6d72a385afdcaafcc08aae65b0
Igor Shcherbak <igorski_is@yahoo.com>
2015-02-16 09:29:32 -0700

    Added chapter 13

---
+++ Chapter_13/ex13extra.rb
@@ -0,0 +1,6 @@
+one, second, third, fourth, ago = ARGV
+
+print 'What is your name? '
+name = STDIN.gets.chomp.capitalize
+
+puts "Hello #{name}\n1st #{one}\n2nd #{second}\n3rd  #{third}\n4th #{fourth}\n5th #{ago}"
